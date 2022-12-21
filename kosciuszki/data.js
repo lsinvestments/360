d7da2ec5be83be4a3bad3dbd50b6f25654f84c7f
lsinvestments <112827422+lsinvestments@users.noreply.github.com>
2022-12-21 21:51:15 +0100

    az

---
+++ kosciuszki/data.js
@@ -0,0 +1,295 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.266714927767854,
+        "pitch": 0.10493655560493309,
+        "fov": 1.5001956320885046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.013982761872053828,
+          "pitch": 0.4065550904631898,
+          "rotation": 0,
+          "target": "1-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kuchnia",
+      "name": "Kuchnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.20456255979044613,
+        "pitch": -0.03356720002951619,
+        "fov": 1.5001956320885046
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.480918570870302,
+          "pitch": 0.14532134918062667,
+          "rotation": 0,
+          "target": "2-hol"
+        },
+        {
+          "yaw": 1.295017897112448,
+          "pitch": -0.04073615265246211,
+          "rotation": 0,
+          "target": "4-azienka"
+        },
+        {
+          "yaw": 2.8587166294732924,
+          "pitch": 0.4488976069781181,
+          "rotation": 0,
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9953222304258134,
+          "pitch": 0.4597838786014101,
+          "title": "Zmywarka",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-hol",
+      "name": "Hol",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.014102816182354871,
+        "pitch": 0.15602070086491437,
+        "fov": 1.5001956320885046
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.099629221262777,
+          "pitch": 0.11117216559887488,
+          "rotation": 0,
+          "target": "5-pokj"
+        },
+        {
+          "yaw": -1.9334946211976867,
+          "pitch": 0.030765550944696685,
+          "rotation": 0,
+          "target": "3-sypialnia"
+        },
+        {
+          "yaw": -1.3755890065694043,
+          "pitch": 0.08166075239899939,
+          "rotation": 0,
+          "target": "4-azienka"
+        },
+        {
+          "yaw": -1.6345291300443847,
+          "pitch": 0.2551002833687459,
+          "rotation": 0,
+          "target": "1-kuchnia"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.25187970538145876,
+          "pitch": 0.13014382280574033,
+          "title": "szafa 1",
+          "text": "Szafa z ukrytą pralką"
+        },
+        {
+          "yaw": -0.685641850310299,
+          "pitch": 0.16816158714979856,
+          "title": "szafa 2 [175cm szerokość]",
+          "text": ""
+        },
+        {
+          "yaw": 1.5571142446769501,
+          "pitch": -0.08179651979114055,
+          "title": "szafa 3 [125cm szerokość]",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.8416871451307735,
+        "pitch": 0.3596406584159766,
+        "fov": 1.5001956320885046
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5933780184195268,
+          "pitch": 0.22020767367798122,
+          "rotation": 0,
+          "target": "1-kuchnia"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4893066324207433,
+          "pitch": 0.060308701653738694,
+          "rotation": 0,
+          "target": "2-hol"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-pokj",
+      "name": "Pokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.46003348301899827,
+        "pitch": 0.0930458916175727,
+        "fov": 1.5001956320885046
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4295405574061464,
+          "pitch": 0.023440992511217118,
+          "rotation": 0,
+          "target": "2-hol"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "pl.Kościuszki",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

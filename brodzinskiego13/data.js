02a5dcc8cffa857ae1481b45497ed5a272157223
lsinvestments <112827422+lsinvestments@users.noreply.github.com>
2023-11-20 14:19:03 +0100

    er

--- brodzinskiego13/data.js
+++ brodzinskiego13/data.js
@@ -1,45 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-azienka",
-      "name": "Łazienka",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1680,
-      "initialViewParameters": {
-        "yaw": 1.7107752045514149,
-        "pitch": 0.04145654961169676,
-        "fov": 1.4898447516704054
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.0785822714247537,
-          "pitch": -0.018017475571980412,
-          "rotation": 0,
-          "target": "1-salon"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-salon",
+      "id": "0-salon",
       "name": "Salon",
       "levels": [
         {
@@ -71,19 +33,19 @@ var APP_DATA = {
           "yaw": 0.7356781740744456,
           "pitch": -0.03745231944288818,
           "rotation": 0,
-          "target": "2-sypialnia"
+          "target": "1-sypialnia"
         },
         {
           "yaw": 1.5516997676676212,
           "pitch": -0.024038103924031162,
           "rotation": 0,
-          "target": "0-azienka"
+          "target": "2-azienka"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-sypialnia",
+      "id": "1-sypialnia",
       "name": "Sypialnia",
       "levels": [
         {
@@ -115,7 +77,45 @@ var APP_DATA = {
           "yaw": 1.3268084418098596,
           "pitch": -0.07877016088086108,
           "rotation": 0,
-          "target": "1-salon"
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.7107752045514149,
+        "pitch": 0.04145654961169676,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0785822714247537,
+          "pitch": -0.018017475571980412,
+          "rotation": 0,
+          "target": "0-salon"
         }
       ],
       "infoHotspots": []

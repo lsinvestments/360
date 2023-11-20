5972da3da070efa5ad5aa0ac77cec9334dde4448
lsinvestments <112827422+lsinvestments@users.noreply.github.com>
2023-11-20 14:34:40 +0100

    s

--- brodzinskiego13/data.js
+++ brodzinskiego13/data.js
@@ -24,66 +24,28 @@ var APP_DATA = {
       ],
       "faceSize": 1680,
       "initialViewParameters": {
-        "yaw": -0.9424775489131747,
-        "pitch": 0.1373244903537163,
+        "yaw": -0.971770507227621,
+        "pitch": 0.1382336819164216,
         "fov": 1.4898447516704054
       },
       "linkHotspots": [
         {
-          "yaw": 0.7356781740744456,
-          "pitch": -0.03745231944288818,
+          "yaw": 0.7380801463340791,
+          "pitch": -0.03817414302183586,
           "rotation": 0,
-          "target": "1-sypialnia"
+          "target": "2-sypialnia"
         },
         {
-          "yaw": 1.5516997676676212,
-          "pitch": -0.024038103924031162,
+          "yaw": 1.552679435642995,
+          "pitch": -0.06760265835483281,
           "rotation": 0,
-          "target": "2-azienka"
+          "target": "1-azienka"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-sypialnia",
-      "name": "Sypialnia",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1680,
-      "initialViewParameters": {
-        "yaw": 2.7793325178276564,
-        "pitch": -0.01868785649920035,
-        "fov": 1.4898447516704054
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.3268084418098596,
-          "pitch": -0.07877016088086108,
-          "rotation": 0,
-          "target": "0-salon"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "2-azienka",
+      "id": "1-azienka",
       "name": "Łazienka",
       "levels": [
         {
@@ -106,14 +68,52 @@ var APP_DATA = {
       ],
       "faceSize": 1680,
       "initialViewParameters": {
-        "yaw": 1.7107752045514149,
-        "pitch": 0.04145654961169676,
+        "yaw": 1.708288592010244,
+        "pitch": 0.06833651971726695,
         "fov": 1.4898447516704054
       },
       "linkHotspots": [
         {
-          "yaw": -3.0785822714247537,
-          "pitch": -0.018017475571980412,
+          "yaw": -3.0879602493268035,
+          "pitch": 0.04209427831222534,
+          "rotation": 0,
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 2.770269451569713,
+        "pitch": 0.25774532806370587,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2869741910900352,
+          "pitch": -0.2933119191918685,
           "rotation": 0,
           "target": "0-salon"
         }

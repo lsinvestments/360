9fcbc0139739533342a86bf00d6eddd2595ae863
lsinvestments <112827422+lsinvestments@users.noreply.github.com>
2023-11-20 16:53:37 +0100

    d

---
+++ brodzinskiego15/data.js
@@ -0,0 +1,187 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.9091457934654361,
+        "pitch": 0.2167009643124338,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3984402516947956,
+          "pitch": 0.3502517429066039,
+          "rotation": 0,
+          "target": "1-kuchania"
+        },
+        {
+          "yaw": 3.127386822396641,
+          "pitch": 0.05478025599870051,
+          "rotation": 0,
+          "target": "2-sypialnia"
+        },
+        {
+          "yaw": 2.628140417504949,
+          "pitch": 0.056295571253308196,
+          "rotation": 0,
+          "target": "3-azienka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-kuchania",
+      "name": "Kuchania",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.7476645252535761,
+        "pitch": 0.12583053363013263,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.00582975019322,
+          "pitch": 0.4374459252932379,
+          "rotation": 0,
+          "target": "0-salon"
+        },
+        {
+          "yaw": -2.953951733889147,
+          "pitch": 0.07599346874134838,
+          "rotation": 0,
+          "target": "2-sypialnia"
+        },
+        {
+          "yaw": 3.0686950929253136,
+          "pitch": 0.08694607873620797,
+          "rotation": 0,
+          "target": "3-azienka"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.11618039716103823,
+        "pitch": 0.34666124717874247,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.014275418826628,
+          "pitch": 0.049116293056897575,
+          "rotation": 0,
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-azienka",
+      "name": "Łazienka",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.989526212783094,
+        "pitch": 0.21905598633254542,
+        "fov": 1.4898447516704054
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1041201807852676,
+          "pitch": 0.028518785326919982,
+          "rotation": 0,
+          "target": "0-salon"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Brodzinskiego M15",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
